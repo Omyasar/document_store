e3a8e2e38505a6353d5c5e43e493856c86ec2560
omyasar <onur.yasar@student.hu.nl>
2023-03-06 10:20:46 +0100

    list comprehension

--- mongo_test.py
+++ mongo_test.py
@@ -3,7 +3,9 @@ import psycopg2
 
 client = MongoClient()
 print(client) #mongoclient
-print(client.sp_db)
-print(client.sp_db.products.find_one()) #eerste product
+print(client.sp_db.products.find())
 
+producten = [product for product in client.sp_db.products.find()]
+print(f'er zijn in toaal {len(producten)} producten')
 
+print(f'het eerste product is {(client.sp_db.products.find_one())}')
