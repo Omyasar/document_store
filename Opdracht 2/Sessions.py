251d656e154bb59bf06bbced4b20fa453eb5f3b3
omyasar <onur.yasar@student.hu.nl>
2023-03-19 15:28:30 +0100

    sessions.py wijz

--- Opdracht 2/Sessions.py
+++ Opdracht 2/Sessions.py
@@ -1,13 +1,10 @@
-import psycopg2
 from pymongo import MongoClient
+import psycopg2
 
-# Globale variabele voor mongodb
-CLIENT = MongoClient()
-mongo_db = CLIENT["huwebshop"]
-profile_collection = mongo_db['profiles']
-session_collection = mongo_db['sessions']
+client = MongoClient()
+db = client["huwebshop"]
+session_collection = db['sessions']
 
-# Postgres connectie maken
 psql_conn = psycopg2.connect(
     host="localhost",
     database="document_store",
@@ -15,25 +12,33 @@ psql_conn = psycopg2.connect(
     password="farmainterim",
     port=5433)
 
-test_profiles = []
 psql_cursor = psql_conn.cursor()
 
-
-def all_sessions_info():
-    for i,doc in enumerate(session_collection.find()):
-        if i >= 1000:
-            break
-        try:
-            session_sale = doc['has_sale']
-        except KeyError:
-            continue
-        test_profiles.append({'has_sale': session_sale, })
-        psql_cursor.execute(
-            "INSERT INTO profiles VALUES (%s)",
-            (session_sale,)
-        )
-        psql_conn.commit()
-    print('Database succesvol gevuld. :)')
+test_sessions = []
 
 
-all_sessions_info()
+# get all product data
+def get_sessions_data():
+    for i, doc in enumerate(session_collection.find()):
+        if i >= 10:
+            try:
+                session_id = doc['_id']
+                session_buid = doc['buid']
+                session_start = doc['session_start']
+                session_end = doc['session_end']
+                session_has_sale = doc['has_sale']
+                sessions_segment = doc['segment']
+                session_preferences = doc['preferences']
+            except KeyError:
+                continue
+            test_sessions.append(
+                {'_id': session_id, 'buid': session_buid, 'session_start': session_start,
+                 'session_end': session_end, 'has_sale': session_has_sale,
+                 'segment': sessions_segment, 'preferences': session_preferences
+                 })
+            print(test_sessions)
+
+
+get_sessions_data()
+
+# insert into database
